fb59c589573b9832cb7076646f0662f1e7363a78
Pravallika <pravallika.p@everest.engineering>
2022-02-04 13:31:22 +0530

    Rename Employee Repository

--- src/main/java/com/example/employeePortal/repositories/EmployeeRepository.java
+++ src/main/java/com/example/employeePortal/repositories/EmployeeRepository.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
-public class EmployeeInMemoryRepository {
+public class EmployeeRepository {
     private AtomicLong NEXT_ID = new AtomicLong(0);
     private Map<Long, Employee> EMPLOYEES = new HashMap<>();
 
