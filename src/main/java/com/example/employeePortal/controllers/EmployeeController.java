1f365418fd132f2b4d5231fe880a7d369e83983a
Pravallika <pravallika.p@everest.engineering>
2022-02-07 11:38:00 +0530

    Save employee

--- src/main/java/com/example/employeePortal/controllers/EmployeeController.java
+++ src/main/java/com/example/employeePortal/controllers/EmployeeController.java
@@ -6,9 +6,10 @@ import com.example.employeePortal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,32 +17,10 @@ import java.util.List;
 public class EmployeeController {
     private final EmployeeService employeeService;
 
-    @GetMapping(value = "")
-    public List<Employee> getAllEmployees() {
-        return employeeService.getAllEmployees();
-    }
-
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable(name = "id") Long id) {
-        Employee employee = employeeService.getEmployeeById(id);
-        if (employee != null)
-            return ResponseEntity.ok(employee);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-    }
-
     @PostMapping("")
     public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
         final Employee newEmployee = employeeService.addEmployee(employee);
         return ResponseEntity.status(HttpStatus.CREATED).body(newEmployee);
     }
 
-    @PutMapping("/{id}")
-    public Employee updateEmployee(@PathVariable(name = "id") Long id, @RequestBody Employee employee) {
-        return employeeService.updateEmployee(id, employee);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteEmployee(@PathVariable(name = "id") Long id) {
-        employeeService.deleteEmployee(id);
-    }
 }
