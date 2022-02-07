b49b3e29b8f9484bff8bb67e56a59c6ec907f2d2
Pravallika <pravallika.p@everest.engineering>
2022-02-07 14:36:26 +0530

    Rename controller

--- src/main/java/com/example/employeePortal/controllers/EmployeeController.java
+++ src/main/java/com/example/employeePortal/controllers/EmployeeController.java
@@ -2,7 +2,7 @@ package com.example.employeePortal.controllers;
 
 
 import com.example.employeePortal.entities.Employee;
-import com.example.employeePortal.repositories.EmployeeInMemoryRepository;
+import com.example.employeePortal.repositories.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,30 +12,30 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/employees")
 public class EmployeeController {
-    private final EmployeeInMemoryRepository employeeInMemoryRepository;
+    private final EmployeeRepository employeeRepository;
 
     @GetMapping(value = "")
     public List<Employee> getAllEmployees() {
-        return employeeInMemoryRepository.getAllEmployees();
+        return employeeRepository.getAllEmployees();
     }
 
     @GetMapping(value = "/{id}")
     public Employee getEmployeeById(@PathVariable(name = "id") Long id) {
-        return employeeInMemoryRepository.getEmployeeById(id);
+        return employeeRepository.getEmployeeById(id);
     }
 
     @PostMapping("")
     public Employee addEmployee(@RequestBody Employee employee) {
-       return employeeInMemoryRepository.addEmployee(employee);
+        return employeeRepository.addEmployee(employee);
     }
 
     @PutMapping("/{id}")
     public Employee updateEmployee(@PathVariable(name = "id") Long id, @RequestBody Employee employee) {
-        return employeeInMemoryRepository.updateEmployee(id,employee);
+        return employeeRepository.updateEmployee(id, employee);
     }
 
     @DeleteMapping("/{id}")
     public void deleteEmployee(@PathVariable(name = "id") Long id) {
-       employeeInMemoryRepository.deleteEmployee(id);
+        employeeRepository.deleteEmployee(id);
     }
 }
