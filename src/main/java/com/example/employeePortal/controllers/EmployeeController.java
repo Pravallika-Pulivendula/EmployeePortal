cce7fc4d07a1910606d9454aa69f4270fc32226d
Pravallika <pravallika.p@everest.engineering>
2022-02-07 11:47:46 +0530

    Update employee

--- src/main/java/com/example/employeePortal/controllers/EmployeeController.java
+++ src/main/java/com/example/employeePortal/controllers/EmployeeController.java
@@ -4,11 +4,8 @@ package com.example.employeePortal.controllers;
 import com.example.employeePortal.entities.Employee;
 import com.example.employeePortal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,32 +13,9 @@ import java.util.List;
 public class EmployeeController {
     private final EmployeeService employeeService;
 
-    @GetMapping(value = "")
-    public List<Employee> getAllEmployees() {
-        return employeeService.getAllEmployees();
-    }
-
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable(name = "id") Long id) {
-        Employee employee = employeeService.getEmployeeById(id);
-        if (employee != null)
-            return ResponseEntity.ok(employee);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-    }
-
-    @PostMapping("")
-    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
-        final Employee newEmployee = employeeService.addEmployee(employee);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newEmployee);
-    }
-
     @PutMapping("/{id}")
-    public Employee updateEmployee(@PathVariable(name = "id") Long id, @RequestBody Employee employee) {
-        return employeeService.updateEmployee(id, employee);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteEmployee(@PathVariable(name = "id") Long id) {
-        employeeService.deleteEmployee(id);
+    public ResponseEntity<Employee> updateEmployee(@PathVariable(name = "id") Long id, @RequestBody Employee employee) {
+        Employee newEmployee = employeeService.updateEmployee(id, employee);
+        return ResponseEntity.ok(newEmployee);
     }
 }
