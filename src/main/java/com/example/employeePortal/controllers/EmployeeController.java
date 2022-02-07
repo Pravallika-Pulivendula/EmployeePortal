eb5c373cd7a467eb570d7a7daa8efded8f431beb
Pravallika <pravallika.p@everest.engineering>
2022-02-07 11:36:44 +0530

    CRUD Operations

--- src/main/java/com/example/employeePortal/controllers/EmployeeController.java
+++ src/main/java/com/example/employeePortal/controllers/EmployeeController.java
@@ -6,16 +6,20 @@ import com.example.employeePortal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/employees")
 public class EmployeeController {
     private final EmployeeService employeeService;
+
+    @GetMapping(value = "")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable(name = "id") Long id) {
@@ -25,4 +29,19 @@ public class EmployeeController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
+    @PostMapping("")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        final Employee newEmployee = employeeService.addEmployee(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newEmployee);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable(name = "id") Long id, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable(name = "id") Long id) {
+        employeeService.deleteEmployee(id);
+    }
 }
