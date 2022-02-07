72af1a91c2a30ddddcfe9e6eae0d034c3b1322b0
Pravallika <pravallika.p@everest.engineering>
2022-02-07 11:28:52 +0530

    Fetch all employees

--- src/main/java/com/example/employeePortal/controllers/EmployeeController.java
+++ src/main/java/com/example/employeePortal/controllers/EmployeeController.java
@@ -4,12 +4,11 @@ package com.example.employeePortal.controllers;
 import com.example.employeePortal.entities.Employee;
 import com.example.employeePortal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,12 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     private final EmployeeService employeeService;
 
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable(name = "id") Long id) {
-        Employee employee = employeeService.getEmployeeById(id);
-        if (employee != null)
-            return ResponseEntity.ok(employee);
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    @GetMapping(value = "")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
     }
 
 }
--- src/main/java/com/example/employeePortal/services/EmployeeService.java
+++ src/main/java/com/example/employeePortal/services/EmployeeService.java
@@ -3,15 +3,19 @@ package com.example.employeePortal.services;
 import com.example.employeePortal.entities.Employee;
 import com.example.employeePortal.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
     @Autowired
     public EmployeeRepository employeeRepository;
 
-    public Employee getEmployeeById(Long id) {
-        return employeeRepository.findById(id).orElse(null);
+    public List<Employee> getAllEmployees() {
+        Sort sort = Sort.by(Sort.Direction.ASC, "doj", "firstName");
+        return employeeRepository.findAll(sort);
     }
 
 }
