ebcc523d08ebc1006b2a3c415bf90b58adc189d4
Pravallika <pravallika.p@everest.engineering>
2022-02-07 11:21:03 +0530

    Fetch employee by id

--- src/main/java/com/example/employeePortal/controllers/EmployeeController.java
+++ src/main/java/com/example/employeePortal/controllers/EmployeeController.java
@@ -4,11 +4,12 @@ package com.example.employeePortal.controllers;
 import com.example.employeePortal.entities.Employee;
 import com.example.employeePortal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +17,12 @@ import java.util.List;
 public class EmployeeController {
     private final EmployeeService employeeService;
 
-    @GetMapping(value = "")
-    public List<Employee> getAllEmployees() {
-        return employeeService.getAllEmployees();
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable(name = "id") Long id) {
+        Employee employee = employeeService.getEmployeeById(id);
+        if (employee != null)
+            return ResponseEntity.ok(employee);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
+
 }
--- src/main/java/com/example/employeePortal/services/EmployeeService.java
+++ src/main/java/com/example/employeePortal/services/EmployeeService.java
@@ -3,19 +3,16 @@ package com.example.employeePortal.services;
 import com.example.employeePortal.entities.Employee;
 import com.example.employeePortal.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class EmployeeService {
     @Autowired
     public EmployeeRepository employeeRepository;
 
-    public List<Employee> getAllEmployees() {
-        Sort sort = Sort.by(Sort.Direction.ASC, "doj", "firstName");
-        return employeeRepository.findAll(sort);
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
     }
+
 }
 
