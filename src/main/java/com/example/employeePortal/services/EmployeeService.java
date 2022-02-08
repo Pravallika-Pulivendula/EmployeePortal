a8ba7e2ac03933a45c31471a6cac1f2f59666160
Pravallika <pravallika.p@everest.engineering>
2022-02-08 10:03:36 +0530

    Use constructor injection

--- src/main/java/com/example/employeePortal/services/EmployeeService.java
+++ src/main/java/com/example/employeePortal/services/EmployeeService.java
@@ -2,17 +2,15 @@ package com.example.employeePortal.services;
 
 import com.example.employeePortal.entities.Employee;
 import com.example.employeePortal.repositories.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
-
-    public EmployeeService(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
-    }
 
     public Employee addEmployee(Employee employee) {
         employeeRepository.save(employee);
