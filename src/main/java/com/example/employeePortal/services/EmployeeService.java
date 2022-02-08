b0b60f700ef40025bb422d4b12147d9379324846
Pravallika <pravallika.p@everest.engineering>
2022-02-08 09:35:01 +0530

    Add transactional annotation

--- src/main/java/com/example/employeePortal/services/EmployeeService.java
+++ src/main/java/com/example/employeePortal/services/EmployeeService.java
@@ -2,19 +2,23 @@ package com.example.employeePortal.services;
 
 import com.example.employeePortal.entities.Employee;
 import com.example.employeePortal.repositories.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 @Service
 public class EmployeeService {
-    @Autowired
-    public EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     public List<Employee> getAllEmployees() {
-        Sort sort = Sort.by(Sort.Direction.ASC, "doj", "firstName","lastName");
+        Sort sort = Sort.by(Sort.Direction.ASC, "doj", "firstName", "lastName");
         return employeeRepository.findAll(sort);
     }
 
