de2d661017dc955280d808b0fcf30019ec7c581a
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-02-16 10:44:56 +0530

    Validate request params

--- src/main/java/com/example/employeePortal/controllers/EmployeeController.java
+++ src/main/java/com/example/employeePortal/controllers/EmployeeController.java
@@ -5,8 +5,12 @@ import com.example.employeePortal.entities.Employee;
 import com.example.employeePortal.exceptions.EmployeeNotFoundException;
 import com.example.employeePortal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @RestController
@@ -16,10 +20,10 @@ public class EmployeeController {
     private final EmployeeService employeeService;
 
     @GetMapping(value = "/search")
-    public List<Employee> findByName(@RequestParam("query") String searchKeyword) {
+    public List<Employee> findByName(@RequestParam("query") @NotBlank String searchKeyword) {
         List<Employee> employeesList = employeeService.findByName(searchKeyword);
-        if(employeesList.isEmpty()){
-            throw new EmployeeNotFoundException("No employee found with the name like: "+searchKeyword);
+        if (employeesList.isEmpty()) {
+            throw new EmployeeNotFoundException("No employee found with the name like: " + searchKeyword);
         }
         return employeesList;
     }
