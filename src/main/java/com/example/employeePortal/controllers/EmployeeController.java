11a75607f149072285c0d1002b248396bc6f20b2
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-02-16 12:32:44 +0530

    Remove custom exception

--- src/main/java/com/example/employeePortal/controllers/EmployeeController.java
+++ src/main/java/com/example/employeePortal/controllers/EmployeeController.java
@@ -2,7 +2,6 @@ package com.example.employeePortal.controllers;
 
 
 import com.example.employeePortal.entities.Employee;
-import com.example.employeePortal.exceptions.EmployeeNotFoundException;
 import com.example.employeePortal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,10 +19,6 @@ public class EmployeeController {
 
     @GetMapping(value = "/search")
     public List<Employee> findByName(@RequestParam("query") String searchKeyword) {
-        List<Employee> employeesList = employeeService.findByName(searchKeyword);
-        if (employeesList.isEmpty()) {
-            throw new EmployeeNotFoundException("No employee found with the name containing: " + searchKeyword);
-        }
-        return employeesList;
+        return employeeService.findByName(searchKeyword);
     }
 }
