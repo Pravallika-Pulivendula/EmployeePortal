ae50a44c6aaa5f07fdb44a0765c20a792b6d4b64
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-02-18 11:09:22 +0530

    Add custom json response

--- src/main/java/com/example/employeePortal/models/EmployeeResponse.java
+++ src/main/java/com/example/employeePortal/models/EmployeeResponse.java
@@ -1,2 +1,29 @@
-package com.example.employeePortal.models;public class EmployeeResponse {
+package com.example.employeePortal.models;
+
+import com.example.employeePortal.entities.Employee;
+import lombok.Data;
+import org.springframework.data.domain.Page;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class EmployeeResponse {
+    private List<Employee> data = new ArrayList<>();
+    private long totalElements;
+    private long totalPages;
+    private long pageSize;
+    private long currentPage;
+    private boolean hasNext;
+    private boolean hasPrevious;
+
+    public EmployeeResponse(Page<Employee> employeePage) {
+        this.setData(employeePage.getContent());
+        this.setTotalElements(employeePage.getTotalElements());
+        this.setTotalPages(employeePage.getTotalPages());
+        this.setPageSize(employeePage.getSize());
+        this.setCurrentPage(employeePage.getNumber());
+        this.setHasNext(employeePage.hasNext());
+        this.setHasPrevious(employeePage.hasPrevious());
+    }
 }
