9ba3c780e570b66060eaaa7eec61e72ac40a60c7
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-02-18 11:08:43 +0530

    Add validations

--- src/main/java/com/example/employeePortal/entities/Address.java
+++ src/main/java/com/example/employeePortal/entities/Address.java
@@ -2,12 +2,30 @@ package com.example.employeePortal.entities;
 
 import lombok.Data;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
 @Data
+@Entity
+@Table(name = "addresses")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long addressId;
+
+    @NotBlank
     private String addressLine1;
+
     private String addressLine2;
+
+    @NotBlank
     private String city;
+
+    @NotBlank
     private String state;
+
     private long zipcode;
+
+    @NotBlank
     private String country;
 }
---
+++ src/main/java/com/example/employeePortal/models/EmployeeResponse.java
@@ -0,0 +1,2 @@
+package com.example.employeePortal.models;public class EmployeeResponse {
+}
