9a002e485d75f59fa661c8264686aa5163ba0d9c
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-02-19 14:19:27 +0530

    Update entities

--- src/main/java/com/example/employeePortal/entities/Employee.java
+++ src/main/java/com/example/employeePortal/entities/Employee.java
@@ -7,57 +7,56 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @Entity
 @Setter
 @Getter
-@Table(name = "employees")
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "employees")
 public class Employee {
     @Id
     @Column(name = "emp_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long empId;
 
-    @NotBlank
-    @Column(name = "first_name")
+    @NotBlank(message = "Firstname should not be blank")
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @NotBlank
-    @Column(name = "last_name")
+    @NotBlank(message = "Lastname should not be blank")
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @NotBlank
+    @NotBlank(message = "Email should not be blank")
     @Email
-    @Column(name = "everest_email_id")
+    @Column(name = "everest_email_id", nullable = false)
     private String everestEmailId;
 
-    @NotBlank
-    @Column(name = "password")
+    @Column(name = "password", nullable = false)
     private String password;
 
-    @NotBlank
     @Email
     @Column(name = "personal_email_id")
     private String personalEmailId;
 
-    @Column(name = "dob")
+    @Column(name = "dob", nullable = false)
     private LocalDate dob;
 
-    @Column(name = "doj")
+    @Column(name = "doj", nullable = false)
     private LocalDate doj;
 
-    @NotBlank
-    @Column(name = "designation")
+    @NotBlank(message = "Designation should be blank")
+    @Column(name = "designation", nullable = false)
     private String designation;
 
-    @Column(name = "experience_in_years")
+    @Min(value = 0, message = "Should be great than 0")
+    @Column(name = "experience_in_years", nullable = false)
     private int experienceInYears;
 
-    @NotBlank
     @Column(name = "bio")
     private String bio;
 
