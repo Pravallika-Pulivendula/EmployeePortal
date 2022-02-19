c08d6c91869e28d19dea02a58b0635b3fb5c47af
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-02-19 14:42:12 +0530

    Update controller tests

--- src/test/java/com/example/employeePortal/controllers/EmployeeControllerTest.java
+++ src/test/java/com/example/employeePortal/controllers/EmployeeControllerTest.java
@@ -9,7 +9,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = EmployeeController.class)
@@ -25,9 +27,11 @@ class EmployeeControllerTest {
     void shouldFetchEmployeeById() throws Exception {
         Employee employee = new Employee();
         employee.setEmpId(1);
+        employee.setFirstName("pravallika");
         when(employeeService.getEmployeeById(employee.getEmpId())).thenReturn(employee);
         this.mockMvc.perform(MockMvcRequestBuilders.get("/api/employees/{id}", employee.getEmpId()))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName",equalTo(employee.getFirstName())));
     }
 
     @Test
