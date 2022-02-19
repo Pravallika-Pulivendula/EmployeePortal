d5eff4eaf2fae119238d830df415e07ef2d20aa3
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-02-19 14:16:46 +0530

    Add controller tests

--- src/test/java/com/example/employeePortal/controllers/EmployeeControllerTest.java
+++ src/test/java/com/example/employeePortal/controllers/EmployeeControllerTest.java
@@ -9,9 +9,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @WebMvcTest(controllers = EmployeeController.class)
 class EmployeeControllerTest {
@@ -23,11 +22,18 @@ class EmployeeControllerTest {
     MockMvc mockMvc;
 
     @Test
-    void shouldFindEmployeeById() throws Exception {
+    void shouldFetchEmployeeById() throws Exception {
         Employee employee = new Employee();
         employee.setEmpId(1);
-        given(employeeService.getEmployeeById(employee.getEmpId())).willReturn(employee);
+        when(employeeService.getEmployeeById(employee.getEmpId())).thenReturn(employee);
         this.mockMvc.perform(MockMvcRequestBuilders.get("/api/employees/{id}", employee.getEmpId()))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturn404ErrorWhenNoEmployeeWithGivenIdIsFound() throws Exception {
+        long empId = 2;
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/employees/{id}", empId))
+                .andExpect(status().isNotFound());
     }
 }
