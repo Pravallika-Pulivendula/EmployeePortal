75852d04c3c38e518a101617dc5ce2e2e1262252
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-02-21 12:14:45 +0530

    Use beforeEach annotation

--- src/test/java/com/example/employeePortal/controllers/EmployeeControllerTest.java
+++ src/test/java/com/example/employeePortal/controllers/EmployeeControllerTest.java
@@ -1,8 +1,10 @@
 package com.example.employeePortal.controllers;
 
+import com.example.employeePortal.entities.Address;
 import com.example.employeePortal.entities.Employee;
 import com.example.employeePortal.services.EmployeeService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,6 +12,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDate;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
@@ -30,12 +34,17 @@ class EmployeeControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private Employee newEmployee;
+
+    @BeforeEach
+    void setUp() {
+        Address presentAddress = new Address(1, "3-116", "ys venkat reddy nagar", "pulivendula", "ap", 516390, "in");
+        Address permanentAddress = new Address(2, "3-116", "ys venkat reddy nagar", "pulivendula", "ap", 516390, "in");
+        newEmployee = new Employee(1, "prav", "reddy", "prav@gmail.com", "pravk", "pravallika@gmail.com", LocalDate.of(2000, 7, 7), LocalDate.of(2021, 8, 2), "se", 1, "hello", presentAddress, permanentAddress);
+    }
+
     @Test
     void shouldCreateNewEmployee() throws Exception {
-        Employee newEmployee = new Employee();
-        newEmployee.setFirstName("pravallika");
-        newEmployee.setLastName("reddy");
-
         given(employeeService.addEmployee(any(Employee.class))).willAnswer((invocation) -> invocation.getArgument(0));
 
         this.mockMvc.perform(MockMvcRequestBuilders.post("/api/employees")
