d1be0e1ba1f178a0a066cedf2ee66bf830bbef23
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-02-21 12:01:08 +0530

    Add validations for entitites

--- src/main/java/com/example/employeePortal/entities/Address.java
+++ src/main/java/com/example/employeePortal/entities/Address.java
@@ -1,6 +1,8 @@
 package com.example.employeePortal.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,25 +11,32 @@ import javax.validation.constraints.NotBlank;
 @Setter
 @Getter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "addresses")
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long addressId;
 
-    @NotBlank
+    @NotBlank(message = "Address should not be blank")
+    @Column(nullable = false)
     private String addressLine1;
 
     private String addressLine2;
 
-    @NotBlank
+    @NotBlank(message = "City should not be blank")
+    @Column(nullable = false)
     private String city;
 
-    @NotBlank
+    @NotBlank(message = "State should not be blank")
+    @Column(nullable = false)
     private String state;
 
+    @Column(nullable = false, length = 20)
     private long zipcode;
 
-    @NotBlank
+    @NotBlank(message = "Country should not be blank")
+    @Column(nullable = false)
     private String country;
 }
