2426e0311063b948d620e2c362a55dba4cb00811
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-05-11 14:16:15 +0530

    exclude datasource auto config

--- src/main/java/com/example/employeePortal/EmployeePortalApplication.java
+++ src/main/java/com/example/employeePortal/EmployeePortalApplication.java
@@ -2,8 +2,9 @@ package com.example.employeePortal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class EmployeePortalApplication {
 
 	public static void main(String[] args) {
