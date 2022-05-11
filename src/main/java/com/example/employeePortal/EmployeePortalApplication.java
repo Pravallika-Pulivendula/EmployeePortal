d5a1d701ca75a733057dcc8f1e2f754c880c324f
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-05-11 15:42:04 +0530

    disable auto config

--- src/main/java/com/example/employeePortal/EmployeePortalApplication.java
+++ src/main/java/com/example/employeePortal/EmployeePortalApplication.java
@@ -1,10 +1,12 @@
 package com.example.employeePortal;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class EmployeePortalApplication {
 
 	public static void main(String[] args) {
